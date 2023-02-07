c63c36222f82faf409bb0df64409b5702ce55a2b
RF1337 <112627787+RF1337@users.noreply.github.com>
2023-02-08 00:48:58 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Robots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                       //Colour, microchip, wifi, bc, wheels, soap container
+            Robot robot = new Robot("white", false, false, 255, 5, 2.7f);
+            Robot cleaningRobot = new Robot("green", false, false, 0, 5, 2.7f);
+        }
+    }
+}
---
+++ Robot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots
+{
+    class Robot
+    {
+        //Creating the different variables for the robots and giving some a default value
+        String colour = "white";
+        bool microchip;
+        bool wifi;
+        byte batteryCapacity = 255;
+        byte wheels = 0;
+        float soapContainer = 2.7f;
+
+        //Creating our constructor
+        public Robot(String colour, bool microchip, bool wifi, byte batteryCapacity, byte wheels, float soapContainer)
+        {
+            this.colour = colour;
+            this.microchip = microchip;
+            this.wifi = wifi;
+            this.batteryCapacity = batteryCapacity;
+            this.wheels = wheels;
+            this.soapContainer = soapContainer;
+        }
+    }
+}
