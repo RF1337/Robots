4dccc4fe5537e5e7aa1a6a207db8ea4b85392a33
RF1337 <112627787+RF1337@users.noreply.github.com>
2023-02-08 12:15:50 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Robots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Robot standardRobot = new Robot("black", "QT8339", 3);
+            Robot wifiRobot = new Robot("yellow", "QT8339", false, 2);
+            Robot batteryRobot = new Robot("black", "QT8339", 200, 4);
+            Robot cleaningRobot = new Robot("black", "QT8339", 1.7f, 5);
+            Robot smallRobot = new Robot("black", "QT8339");
+
+
+
+
+        }
+    }
+}
---
+++ Robot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots
+{
+    class Robot
+    {
+        //Standard
+        public Robot(string colour, string microchip, byte wheels)
+        {
+            colour = "white";
+            Console.WriteLine(colour + " " + microchip);
+        }
+
+        //With wifi
+        public Robot(string colour, string microchip, bool hasWifi, byte wheels) : this(colour, microchip, wheels)
+        {
+            hasWifi = true;
+            Console.WriteLine(colour + " " + microchip + " " + hasWifi + " " + wheels);
+        }
+
+        //With battery
+        public Robot(string colour, string microchip, byte batteryCapacity, byte wheels)
+        {
+            colour = "white";
+            Console.WriteLine(colour + " " + microchip + " " + batteryCapacity + " " + wheels);
+        }
+
+        //Cleaning robot
+        public Robot(string colour, string microchip, float soapContainer, byte wheels) 
+        {
+            soapContainer = 2.7f;
+            Console.WriteLine(colour + " " + microchip + " " + soapContainer + " " + wheels);
+        }
+
+        //Small robot
+        public Robot(string colour, string microchip) 
+        {
+            colour = "white";
+            Console.WriteLine(colour + " " + microchip);
+        }
+    }
+}
